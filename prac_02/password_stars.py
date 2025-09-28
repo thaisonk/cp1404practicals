8f361aeef4aed9ec0b0534e10f6edfda9785f351
thaisonk <thaison.kinnane@my.jcu.edu.au>
2025-09-28 18:22:11 +1000

    Refactor password check and asterisk printing to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,17 +2,19 @@
 
 def main():
     MIN_LENGTH = 6
-
+    """Get password and print using functions"""
     password = get_password(MIN_LENGTH)
 
     print_asterisks(password)
 
 
 def print_asterisks(password: str):
+    """Print number of asterisks for characters in password."""
     print("*" * len(password))
 
 
 def get_password(MIN_LENGTH: int) -> str:
+    """Get password with minimum length required."""
     password = input("Enter a password: ")
     while len(password) < MIN_LENGTH:
         print(f"Password must be at least {MIN_LENGTH} characters long.")
