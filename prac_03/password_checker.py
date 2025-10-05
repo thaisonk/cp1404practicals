ba534c3ccbec3730355aad00354738e535d782c4
thaisonk <thaison.kinnane@my.jcu.edu.au>
2025-10-05 19:12:00 +1000

    Implement password checker with full validation rules

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -5,7 +5,7 @@ Password checker "skeleton" code to help you get started
 
 MIN_LENGTH = 2
 MAX_LENGTH = 6
-IS_SPECIAL_CHARACTER_REQUIRED = False
+IS_SPECIAL_CHARACTER_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
@@ -27,7 +27,7 @@ def main():
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
-    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
         return False
 
     number_of_lower = 0
@@ -43,12 +43,12 @@ def is_valid_password(password):
             number_of_digit += 1
         elif character in SPECIAL_CHARACTERS:
             number_of_special += 1
-        pass
 
-    # TODO: if any of the 'normal' counts are zero, return False
+    if number_of_lower == 0 or number_of_upper == 0 or number_of_digit == 0:
+        return False
 
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
+    if IS_SPECIAL_CHARACTER_REQUIRED and number_of_special == 0:
+        return False
 
     # if we get here (without returning False), then the password must be valid
     return True
