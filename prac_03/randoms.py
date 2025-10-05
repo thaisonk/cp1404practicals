bca26d2a16a3484f252e342bbaf7087ce91fd480
thaisonk <thaison.kinnane@my.jcu.edu.au>
2025-10-05 18:12:17 +1000

    Add random number code and commented answers

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+# Line 1:
+# I saw a random number between 5 and 20
+# Smallest possible number was 5
+# Largest possible number was 20
+
+# Line 2:
+# I saw a random odd number between 3 and 9
+# Smallest possible number was 3
+# Largest possible number was 9
+# Could line 2 have produced a 4? No as the step is 2 so only odd numbers are given
+
+# Line 3:
+# I saw a random floating point number between 2.5 and 5.5.
+# Smallest possible number was 2.5
+# Largest possible number was 5.5
+
+import random
+
+print(random.randint(1,100))
