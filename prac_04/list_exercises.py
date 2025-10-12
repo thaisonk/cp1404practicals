9f3fcd766f5bcdf0aabc5b384565949f99d1db95
thaisonk <thaison.kinnane@my.jcu.edu.au>
2025-10-12 22:06:00 +1000

    Add username access check

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -11,3 +11,13 @@ print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 average = sum(numbers) / len(numbers)
 print(f"The average of the numbers is {average}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter username:")
+
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
