bcaa56a3d11510fa6c2b4ce7d7a1ee070c307367
thaisonk <thaison.kinnane@my.jcu.edu.au>
2025-10-19 15:00:17 +1000

    Add email program to store user email and name

---
+++ prac_05/emails.py
@@ -0,0 +1,29 @@
+"""
+Emails
+Estimate: 20 minutes
+Actual: 27 minutes
+"""
+
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ").strip().upper()
+        if confirmation != "Y" and confirmation != "":
+            name = input("Name: ").strip()
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+def get_name_from_email(email):
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
+if __name__ == "__main__":
+    main()
+
